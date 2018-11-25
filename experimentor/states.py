059f4f61d830f530ae4d7b3739c8d5c8b5a210e2
jmosbacher <joe.mosbacher@gmail.com>
2018-11-25 18:37:26 +0200

    not working yet

--- experimentor/states.py
+++ experimentor/states.py
@@ -1,13 +1,21 @@
 from itertools import product
 from collections import defaultdict
 import time
+from .iterators import Map, Eval, IterExpression
+
+
+def lazy_product(iters):
+    if not iters:
+        yield ()
+    else:
+        for it in iters[0]:
+            for rest in lazy_product(iters[1:]):
+                yield (it,) + rest
 
 class States:
-    def __init__(self, iterators, maps, evaluated, excluded):
+    def __init__(self, iterators, excluded=()):
         self.iterators = iterators
-        self.maps = maps
-        self.evaluated = evaluated
-        self.excluded = excluded
+        self.shared_state = {}
 
     @classmethod
     def from_config_file(cls, path):
@@ -21,85 +29,49 @@ class States:
         for name, expr in config['iterators'].items():
             dev, attr = name.split('.')
             alias = aliases.get(name, name)
-            it = [(dev, attr, alias, val) for val in eval(expr)]
+            it = IterExpression(dev, attr, alias, expr)
             iterators.append(it)
 
-        maps = {
-            "multi": [],
-            "single": []
-        }
-        for name, kind in config['mappings'].items():
+
+        for name, nmax in config['maps'].items():
             dev, attr = name.split('.')
             alias = aliases.get(name, name)
             map = dict(config[name])
-            maps[kind].append((dev, attr, alias, map))
+            m = Map(dev, attr, alias, map)
+            iterators.append(map)
+
 
         evaluated = []
         for name, expr in config['evaluated'].items():
             dev, attr = name.split('.')
             alias = aliases.get(name, name)
+
             evaluated.append((dev, attr, alias, expr))
 
         excluded = []
         for name, expr in config['excluded'].items():
-            dev, attr = name.split('.')
-            alias = aliases.get(name, name)
-            excluded.append((dev, attr, alias, expr))
+            excluded.append((name, expr))
 
-        return cls(iterators, maps, evaluated, excluded)
+        return cls(iterators, tuple(excluded))
 
-
-    def states(self):
-        return [s for s in self.iter_states()]
-
-    def iter_states(self):
-        shared = {'state_idx': 0}
-
-        # Iterate over the external product of iterators
-        for iparams in product(*self.iterators):
-            shared['timestamp'] = int(time.time())
-            state = defaultdict(dict)
-            for dev, attr, alias, val in iparams:
-                shared[alias] = val
-                state[dev][attr] = val
-
-            # set the mapped parameters
-
-            for dev, attr, alias, mapping in self.maps["single"]:
-                for expr, val in mapping.items():
-                    if eval(expr.format(**shared)):
-                        shared[alias] = val
-                        state[dev][attr] = val
-                        break
-
-            for dev, attr, alias, expr in self.evaluated:
-                val = eval(expr.format(**shared))
-                shared[alias] = val
-                state[dev][attr] = val
-
-            if any([eval(expr.format(**shared)) for expr in self.excluded]):
-                continue
-
-            maps = []
-            for dev, attr, alias, mapping in self.maps["multi"]:
-                map = []
-                for expr, val in mapping.items():
-                    if eval(expr.format(**shared)):
-                        map.append((dev, attr, alias, val))
-                maps.append(map)
-
-            states = []
-            for mparams in product(*maps):
-                for dev, attr, alias, val in mparams:
-                    s = {k:v for k,v in state.items()}
-                    s[dev][attr] = val
-                    states.append(s)
-            if not states:
-                states.append(state)
-
-            for state in states:
-                shared['state_idx'] += 1
-                yield state
 
     def __iter__(self):
-        return self.iter_states()
+        self.shared_state['state_idx'] = 0
+        self.shared_state['timestamp'] = int(time.time())
+
+        for params in lazy_product(self.iterators):
+            state = defaultdict(dict)
+            for dev, attr, val in params:
+                state[dev][attr] = val
+
+            for name, expr in self.excluded:
+                if eval(expr.format(**self.shared_state)):
+                    #self.logger.info(f"skipping state due to {name}")
+                    break
+            else:
+                self.shared_state['state_idx'] += 1
+                yield state
+
+            self.shared_state['timestamp'] = int(time.time())
+
+
